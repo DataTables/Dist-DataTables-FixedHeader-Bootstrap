f6d04d4732277dbaef1936445595e25c494aaa44
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-07 16:17:14 +0000

    43cfde4f36a20955a840ca5301328839c2337bff Release 3.3.1: Build error so releasing
    Sync to source repo @43cfde4f36a20955a840ca5301328839c2337bff

--- js/fixedHeader.bootstrap.mjs
+++ js/fixedHeader.bootstrap.mjs
@@ -1,11 +1,10 @@
-
 /*! Bootstrap 3 styling wrapper for FixedHeader
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-bs';
-import DataTable from 'datatables.net-fixedheader';
+import 'datatables.net-fixedheader';
 
 
 
