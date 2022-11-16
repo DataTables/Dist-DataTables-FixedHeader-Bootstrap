ce1aa9a2056b524d8b96e1def510af12b9b4e54b
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:07:25 +0000

    43cfde4f36a20955a840ca5301328839c2337bff
    Sync to source repo @3cfde4f36a20955a840ca5301328839c2337bff

--- js/fixedHeader.bootstrap.min.mjs
+++ js/fixedHeader.bootstrap.min.mjs
@@ -1,4 +1,4 @@
 /*! Bootstrap 3 styling wrapper for FixedHeader
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-bs";import"datatables.net-fixedheader";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-bs";import FixedHeader from"datatables.net-fixedheader";export default DataTable;
